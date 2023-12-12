f9a1c5a05e17ce603d9a2d69cf41177b477afe58
Mark Fairhurst <mark@fairhursts.net>
2023-12-12 18:38:37 +0000

    feat: Add masonry

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,39 @@
+"use client";
+
 import Image from "next/image";
+import { Masonry } from "react-plock";
+
+// TODO: fetch image urls from DB
+const images = [
+  {
+    src: "https://ik.imagekit.io/mfimages/ella/ella-110.jpg",
+    alt: "Ella gallery",
+  },
+  {
+    src: "https://ik.imagekit.io/mfimages/toby/toby-031.jpg",
+    alt: "Toby gallery",
+  },
+  {
+    src: "https://ik.imagekit.io/mfimages/alps/alps-2015-037.jpg",
+    alt: "Les Alpes gallery",
+  },
+  {
+    src: "https://ik.imagekit.io/mfimages/dogs/ted.jpg",
+    alt: "Dogs gallery",
+  },
+  {
+    src: "https://ik.imagekit.io/mfimages/lakes/lakes-2011-002.jpg",
+    alt: "Lakes gallery",
+  },
+  {
+    src: "https://ik.imagekit.io/mfimages/venice/venice-070.jpg",
+    alt: "Venice gallery",
+  },
+  {
+    src: "https://ik.imagekit.io/mfimages/ski/meribel-2019-021.jpg",
+    alt: "Ski gallery",
+  },
+];
 
 export default function HomePage() {
   return (
@@ -8,78 +43,25 @@ export default function HomePage() {
           Photos
         </h1>
 
-        <div className="grid max-w-[992] auto-rows-[200px] grid-cols-[repeat(3,minmax(320px,auto))] gap-4">
-          <div className="relative overflow-hidden rounded-lg hover:shadow-lg">
+        <Masonry
+          items={images}
+          config={{
+            columns: [1, 2, 3],
+            gap: [12, 24, 24],
+            media: [640, 768, 1024],
+          }}
+          render={(item) => (
             <Image
-              src="https://ik.imagekit.io/mfimages/ella/ella-110.jpg"
-              fill
-              style={{ objectFit: "cover" }}
-              sizes="(max-width: 768px) 100vw, (max-width: 1200px) 320px, 320px"
-              alt="Ella gallery"
+              key={item.src}
+              src={item.src}
+              width={400}
+              height={300}
+              sizes="(max-width: 640px) 100vw, (max-width: 1280px) 320px, 320px"
+              alt={item.alt}
               priority
             />
-          </div>
-          <div className="relative overflow-hidden rounded-lg hover:shadow-lg">
-            <Image
-              src="https://ik.imagekit.io/mfimages/toby/toby-031.jpg"
-              fill
-              style={{ objectFit: "cover" }}
-              sizes="(max-width: 768px) 100vw, (max-width: 1200px) 320px, 320px"
-              alt="Toby gallery"
-              priority
-            />
-          </div>
-          <div className="relative overflow-hidden rounded-lg hover:shadow-lg">
-            <Image
-              src="https://ik.imagekit.io/mfimages/alps/alps-2015-037.jpg"
-              fill
-              style={{ objectFit: "cover" }}
-              sizes="(max-width: 768px) 100vw, (max-width: 1200px) 320px, 320px"
-              alt="Les Alpes gallery"
-              priority
-            />
-          </div>
-          <div className="relative overflow-hidden rounded-lg hover:shadow-lg">
-            <Image
-              src="https://ik.imagekit.io/mfimages/dogs/ted.jpg"
-              fill
-              style={{ objectFit: "cover" }}
-              sizes="(max-width: 768px) 100vw, (max-width: 1200px) 320px, 320px"
-              alt="Dogs gallery"
-              priority
-            />
-          </div>
-          <div className="relative overflow-hidden rounded-lg hover:shadow-lg">
-            <Image
-              src="https://ik.imagekit.io/mfimages/lakes/lakes-2011-002.jpg"
-              fill
-              style={{ objectFit: "cover" }}
-              sizes="(max-width: 768px) 100vw, (max-width: 1200px) 320px, 320px"
-              alt="Les Alpes gallery"
-              priority
-            />
-          </div>
-          <div className="relative overflow-hidden rounded-lg hover:shadow-lg">
-            <Image
-              src="https://ik.imagekit.io/mfimages/ski/meribel-2019-021.jpg"
-              fill
-              style={{ objectFit: "cover" }}
-              sizes="(max-width: 768px) 100vw, (max-width: 1200px) 320px, 320px"
-              alt="Meribel gallery"
-              priority
-            />
-          </div>
-          <div className="relative overflow-hidden rounded-lg hover:shadow-lg">
-            <Image
-              src="https://ik.imagekit.io/mfimages/venice/venice-070.jpg"
-              fill
-              style={{ objectFit: "cover" }}
-              sizes="(max-width: 768px) 100vw, (max-width: 1200px) 320px, 320px"
-              alt="Venice gallery"
-              priority
-            />
-          </div>
-        </div>
+          )}
+        />
       </div>
     </main>
   );
