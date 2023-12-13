f9217f7d591bff99010f921eb348e9e5b7caf821
Mark Fairhurst <mark.fairhurst@simplybusiness.co.uk>
2023-12-13 13:06:56 +0000

    feat: Add s3 upload

---
+++ src/app/upload/layout.tsx
@@ -0,0 +1,26 @@
+import "@/styles/globals.css";
+
+import { Inter } from "next/font/google";
+
+const inter = Inter({
+  subsets: ["latin"],
+  variable: "--font-sans",
+});
+
+export const metadata = {
+  title: "Lakeland Photos",
+  description: "lakeland.app",
+  icons: [{ rel: "icon", url: "/favicon.ico" }],
+};
+
+export default function Layout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={`font-sans ${inter.variable}`}>{children}</body>
+    </html>
+  );
+}
